837794fb3cff85fe2aaa4f75ac50472e4641345f
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-08-10 08:50:03 +0900

    Add texts for name and profession

--- app/src/main/java/com/jp_funda/profile/MainActivity.kt
+++ app/src/main/java/com/jp_funda/profile/MainActivity.kt
@@ -3,13 +3,19 @@ package com.jp_funda.profile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jp_funda.profile.ui.theme.ProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +24,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProfileTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    Text(text = "山田 太郎", color = Color.Gray)
+                    Text(text = "職業: Androidエンジニア", color = Color.Gray)
                 }
             }
         }
