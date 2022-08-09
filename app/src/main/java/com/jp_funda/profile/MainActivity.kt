53fd06ea3a65c10d7c45bb876e4681c061a79e4e
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-08-10 08:59:22 +0900

    Set font size and font weight

--- app/src/main/java/com/jp_funda/profile/MainActivity.kt
+++ app/src/main/java/com/jp_funda/profile/MainActivity.kt
@@ -4,18 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jp_funda.profile.ui.theme.ProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +30,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    Text(text = "山田 太郎", color = Color.Gray)
-                    Text(text = "職業: Androidエンジニア", color = Color.Gray)
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = "山田 太郎",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = "職業: Androidエンジニア", color = Color.Gray, fontSize = 16.sp)
+                    }
                 }
             }
         }
